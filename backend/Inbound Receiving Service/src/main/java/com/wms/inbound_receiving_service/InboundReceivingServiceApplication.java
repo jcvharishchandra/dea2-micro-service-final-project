2a9a7e4f0fb87c34c173a39fda460963c4c0bceb
Dinesh Rathnasiri <dineshrathnasiri017@gmail.com>
2026-01-28 12:18:07 +0530

    Add initial Spring Boot project for inbound receiving
    Set up a new Spring Boot project named 'inbound-receiving-service' with basic configuration, Maven wrapper, and a sample test. Includes initial dependencies for actuator, validation, and webmvc.

---
+++ backend/Inbound Receiving Service/src/main/java/com/wms/inbound_receiving_service/InboundReceivingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wms.inbound_receiving_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InboundReceivingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InboundReceivingServiceApplication.class, args);
+	}
+
+}
